9ecf71c36742528993e63fe462df8bfcdd177ec1
Korisss <vaginpawel2005@yandex.ru>
2023-03-19 20:54:25 +0300

    fix: change config paths

---
+++ base-config/base.lua
@@ -0,0 +1,36 @@
+vim.cmd([[colorscheme sonokai]])
+
+local opt = vim.opt
+
+opt.fileencoding = 'utf-8'
+opt.fileencodings = 'utf-8'
+
+opt.errorbells = true
+opt.visualbell = true
+
+opt.backup = false
+opt.writebackup = false
+
+opt.linebreak = true
+opt.ruler = true
+opt.scrolloff = 6
+opt.showmatch = true
+opt.textwidth = 80
+opt.undolevels = 1000
+opt.updatetime = 200
+opt.signcolumn = 'yes'
+opt.number = true
+opt.fileformat = 'unix'
+
+opt.hlsearch = true
+opt.ignorecase = true
+opt.incsearch = true
+
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.smartindent = true
+opt.smarttab = true
+opt.autoindent = true
+
